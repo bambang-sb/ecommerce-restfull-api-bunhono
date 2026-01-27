aeb52e0a99801d0ae70752d13d85cf959735cdde
bambang sb <7budisetia@gmail.com>
2026-01-27 22:32:14 +0700

    chore: add logic validation form

---
+++ src/validations/validate.ts
@@ -0,0 +1,25 @@
+import { ValidationError } from "../errors/errors-handle";
+
+const validate = (req: any, schema: any) => {
+  let val = schema.safeParse(req);
+  
+  if (!val.success) {
+    let key="";
+    let err={} as Record<string, string[]>;
+    val.error.issues.forEach((issue:any) => {
+      if(issue.code === 'unrecognized_keys'){
+        err['unknown_field'] ||= [];
+        err['unknown_field'].push(`Unknown field ${issue.keys} !`);
+        return;
+      }
+      key = issue.path.join('.');
+      err[key] ||= [];
+      err[key].push(`${issue.message}`);
+    });
+    throw new ValidationError('Validation Failed ', err, 422);
+  }
+  return val.data;
+}
+export {
+  validate
+};  
