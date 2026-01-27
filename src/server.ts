8a2eb41beeb01b52656582ad1ca710baf2035583
bambang sb <7budisetia@gmail.com>
2026-01-27 22:36:44 +0700

    chore: setup server.ts import globar error adn route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import { Hono } from 'hono'
+import r from './routes/route'
+import errorMiddleware from './middleware/error-middleware'
+import { errorResponse } from './helpers/response';
 
 const app = new Hono()
+
+app.route('/api/',r)
 
 app.get('/', async (c) => {
 
   return c.text('Hello Hono!')
 })
+
+app.notFound(async (c) => {
+  return errorResponse(c, 'Not Found', 404);
+});
+
+app.onError(errorMiddleware)
 
 export default app
