f0ac11b6af52f7195c7d2a5add144b8ed00e59b9
bambang sb <bambangsb2025@gmail.com>
2026-01-28 16:51:48 +0700

    chore(prisma type):  add prisma type

---
+++ src/types/prismaType.ts
@@ -0,0 +1,15 @@
+import {type Prisma } from '../../prisma/generated/prisma/client';
+
+export type UserCreateType = Prisma.UsersCreateInput;
+
+// export type ProductCreateType = Prisma.ProductsCreateInput;
+
+// export type CategoryCreateType = Prisma.CategoriesCreateInput;
+
+// export type AddressCreateType = Prisma.AddressesCreateInput;
+
+// export type CartItemCreateType = Prisma.CartItemsCreateInput;
+
+// export type OrderCreateType = Prisma.OrdersCreateInput;
+
+// export type OrderItemCreateType = Prisma.OrderItemsCreateInput;
