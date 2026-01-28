b79c53523ecc1cc2dd6dcc067862c0cd9e9d37ca
bambang sb <bambangsb2025@gmail.com>
2026-01-28 14:56:03 +0700

    fix(validation): add rule username fro user

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -3,6 +3,7 @@ import * as z from 'zod'
 const userLoginValidation = z.object({
   email: z.string({message: "email is required!"})
     .email({message: "invalid email format!"}),
+  username: z.string({message: "username is required!"}),
   password: z.string({message:'password is required!'})
     .min(3,{message:'password minimal 3 character!'})
     .max(100,{message:'password maximal 100 character!'})
