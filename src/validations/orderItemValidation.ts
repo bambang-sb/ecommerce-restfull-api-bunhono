8b3444ac614b46b647556701cfa8b39c2cc4df6a
bambang sb <bambangsb2025@gmail.com>
2026-01-28 14:41:44 +0700

    chore: add cartItem and orderItem validation rule

---
+++ src/validations/orderItemValidation.ts
@@ -0,0 +1,8 @@
+import * as z from 'zod'
+
+const orderItemValidation = z.object({
+  productId: z.string().min(1, 'Product ID is required'),
+  quantity: z.number().min(1, 'Quantity must be at least 1'),
+})
+
+export default orderItemValidation
