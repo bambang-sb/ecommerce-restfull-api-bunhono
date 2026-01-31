8a5f48eadbd65e310403a5b83dab9ac9624d148d
bambang sb <bambangsb2025@gmail.com>
2026-02-01 00:11:54 +0700

    fix(validation):  add rule order item

--- src/validations/orderItemValidation.ts
+++ src/validations/orderItemValidation.ts
@@ -1,8 +1,10 @@
 import * as z from 'zod'
 
 const orderItemValidation = z.object({
-  productId: z.string().min(1, 'Product ID is required'),
-  quantity: z.number().min(1, 'Quantity must be at least 1'),
+  product: z.number({message:'Product must be number'}).min(1, 'Product must be at last 1'),
+  quantity: z.number({message:'Product must be number'}).min(1, 'Quantity must be at least 1'),
+  orderAddress:z.string({message:'address is required!'}),
+  zipCode:z.number({message:"zip code is required!"}).min(1, 'Product must be at last 1')
 })
 
 export default orderItemValidation
