0d046c446e3ce3ff8bef63514b820331f1c07954
bambang sb <bambangsb2025@gmail.com>
2026-02-03 20:56:25 +0700

    refactor(validation): add rule many items

--- src/validations/orderItemValidation.ts
+++ src/validations/orderItemValidation.ts
@@ -3,8 +3,13 @@ import * as z from 'zod'
 const orderItemValidation = z.object({
   product: z.number({message:'Product must be number'}).min(1, 'Product must be at last 1'),
   quantity: z.number({message:'Product must be number'}).min(1, 'Quantity must be at least 1'),
-  orderAddress:z.string({message:'address is required!'}),
-  zipCode:z.number({message:"zip code is required!"}).min(1, 'Product must be at last 1')
-})
+  cartItem: z.number({message:'Item must be number'}).min(1, 'Item must be at least 1')
+});
 
-export default orderItemValidation
+const orderValidation = z.object({
+  orderAddress:z.string({message:'address is required!'}),
+  zipCode:z.number({message:"zip code is required!"}).min(1, 'Product must be at last 1'),
+  items:z.array(orderItemValidation).min(1,"item cannot be null!")
+});
+
+export default orderValidation
